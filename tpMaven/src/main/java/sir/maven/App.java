3a24a6c0f50e8f4612ca9e936d551586ac05a61f
mdiansow <mdiansow@yahoo.fr>
2014-01-22 18:14:58 +0100

    initial commit

---
+++ tpMaven/src/main/java/sir/maven/App.java
@@ -0,0 +1,35 @@
+package sir.maven;
+
+/**
+ * Hello world!
+ */
+
+public final class App {
+	/**
+	 * Constructeur.
+	 */
+	private App() {
+		// not called
+	}
+
+	/**
+	 * Main.
+	 * @param args String []
+	 */
+	public static void main(final String[] args) {
+		// nb de test
+		int n = 0;
+		System.out.println("Hello World!");
+		if (n != 0) {
+			System.out.println("Coucou");
+		} else {
+			System.out.println("No");
+		}
+
+		if (n != 0) {
+			System.out.println("Coucou");
+		} else {
+			System.out.println("No");
+		}
+	}
+}
